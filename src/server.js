e6141e469ae72d5beb06ff51be41e59414c7fa57
LeonardoCosta90 <leonardoeverton.tec@gmail.com>
2023-10-02 15:54:13 -0300

    feat: http methods

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
- res.end('Hello World')
+    const { method, url } = req
+    
+    if(method === 'GET'){
+        console.log('buscando usuários')
+    } else if(method === 'POST'){
+        console.log('criando usuários')
+    }
+    res.end('Hello World')
 })
 
 server.listen(3333)
